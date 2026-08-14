f5ee01d0a2d23fdffe1bb7acb5816b1cc0bd39bb
hazel <infinitech.hazel@gmail.com>
2026-08-14 11:37:49 +0800

    feat: add vercel analytics package and import in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import type { Metadata } from "next";
 import ClientLayout from "./ClientLayout";
 import ServiceWorkerProvider from "@/components/ServiceWorkerProvider";
+import { Analytics } from "@vercel/analytics/next"
 import "leaflet/dist/leaflet.css"
 import "./globals.css";
 
